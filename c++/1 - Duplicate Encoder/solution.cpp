ffa26eb9a7bd32a1db4f993bc6b1b96800156d8d
NicRiv <nico.a.rivera3@gmail.com>
2022-08-07 00:02:37 -0300

    First exercise

---
+++ c++/1 - Duplicate Encoder/solution.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <cctype>
+
+std::string duplicate_encoder(const std::string& word){
+  std::string new_word;
+  bool aprs;
+  
+  for (unsigned int i = 0; i < word.length(); i++) {
+    aprs = false;
+    
+    for (unsigned int j = 0; j < word.length(); j++) {
+      if ((i != j) && (tolower(word[i]) == tolower(word[j]))) {
+        aprs = true;
+      }
+    }
+    
+    if (aprs) {
+      new_word += ")";
+    } else {
+      new_word += "(";
+    }
+  }
+  return new_word;
+}
