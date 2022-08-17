e5a79ce0045c16a97281288db40875a7f23f7df6
NicRiv <nico.a.rivera3@gmail.com>
2022-08-17 15:39:47 -0300

    Unique in Order, in Python

--- python/2 - Unique In Order/solution.py
+++ python/2 - Unique In Order/solution.py
@@ -3,6 +3,8 @@ Implement the function unique_in_order which takes as argument a sequence and re
 a list of items without any elements with the same value next to each other and preserving 
 the original order of elements.
 
+[Level: 6 kyu]
+
 Example:
     'AAAABBBCCDAABBB' = ['A', 'B', 'C', 'D', 'A', 'B']
             'ABBCcAD' = ['A', 'B', 'C', 'c', 'A', 'D']
