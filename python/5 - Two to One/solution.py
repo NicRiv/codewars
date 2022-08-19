584c966e91bb9e4b5e28039838b27b78b3e3d93c
NicRiv <nico.a.rivera3@gmail.com>
2022-08-19 20:52:14 -0300

    Two to One, in Python

--- python/5 - Two to One/solution.py
+++ python/5 - Two to One/solution.py
@@ -6,6 +6,8 @@ print('\nTwo to One')
 Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, 
 the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
+[Level: 7 kyu]
+
 input: 
     a = "xyaabbbccccdefww"
     b = "xxxxyyyyabklmopq"
